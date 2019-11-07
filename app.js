30e82cfd5df6555d0b078cf2e139fa367d97bc83
jtrcarlos <jrcarlos@outlook.pt>
2019-11-07 22:28:26 +0000

    script + css + particles + rework pagina

---
+++ app.js
@@ -0,0 +1,16 @@
+var titulo = document.getElementById("Titulo");
+
+titulo.addEventListener("click", function() {
+  titulo.style.color = "red";
+});
+// titulo.addEventListener("mouseenter", function() {
+//   titulo.style.color = "blue";
+// });
+
+$("#Titulo").click(function() {
+  $("#Titulo").css("color", "red");
+});
+
+particlesJS.load("particles-js", "particlesjs-config.json", function() {
+  console.log("callback - particles.js config loaded");
+});
