276fa2283ac5450d88db0dcc20d5332e8554f200
leoleandroleo <leandromedeirosbarbosa@hotmail.com>
2019-11-07 23:12:57 +0000

    BUG 2

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var titulo = document.getElementById("Titulo");
+// var titulo = document.getElementById("Titulo");
 
-titulo.addEventListener("click", function() {
-  titulo.style.color = "red";
-});
-// titulo.addEventListener("mouseenter", function() {
-//   titulo.style.color = "blue";
+// titulo.addEventListener("click", function() {
+//   titulo.style.color = "red";
 // });
+// // titulo.addEventListener("mouseenter", function() {
+// //   titulo.style.color = "blue";
+// // });
 
-$("#Titulo").click(function() {
-  $("#Titulo").css("color", "red");
-});
+// $("#Titulo").click(function() {
+//   $("#Titulo").css("color", "red");
+// });
 
 particlesJS.load("particles-js", "particlesjs-config.json", function() {
   console.log("callback - particles.js config loaded");
